e6a96f60be2da6f704103acf54ded19cbc05ff95
Mahmoud3mood <168723599+Mahmoud3mood@users.noreply.github.com>
2024-05-08 17:06:18 +0000

    CODE CHALLENGE 01 ...//parseInt//parseFloat

--- src/03_prompt.js
+++ src/03_prompt.js
@@ -1,6 +1,16 @@
-const prompt = require('prompt-sync')({sigint: true});
 
-// Alles muss man selber machen lassen! ...
-let firstName = prompt("Vorname?: ");
+/*
+const prompt = require('prompt-sync')({sigint: true});
+// CODE CHALLENGE 01 ...
+let firstName = prompt("vorName?: ");
 let familyName = prompt("Name?: ");
 console.log("Hallo, " + firstName + " " + familyName +"!");
+*/
+
+
+const prompt = require('prompt-sync')({sigint: true});
+// CODE CHALLENGE 01 ...//parseInt//parseFloat
+let firstNumber =  parseInt(prompt("Geben Sie die erste Zahl ein: "));
+let secondNumber = parseInt(prompt("Geben Sie die zweite Zahl ein: "));
+let Summe = (firstNumber + secondNumber);
+console.log("Die Summe der Zahlen ist:", Summe);
