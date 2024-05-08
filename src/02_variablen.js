9b61a179f62cdf0b5bb44a316c3be7d383aa41e0
Mahmoud3mood <168723599+Mahmoud3mood@users.noreply.github.com>
2024-05-08 10:02:18 +0000

    // variable vs. kon

--- src/02_variablen.js
+++ src/02_variablen.js
@@ -9,10 +9,53 @@
 */
 // console.log("HI")
 // console.log(HI)
-
+/*
 let firstName; // Deklaration ( Definition)
 console.log(firstName); // Ausgabe
 firstName = "Max"; // wertzuweisung - assignment
 console.log(firstName); // Ausgabe
 let familyName = "Mütze";
 console.log("Hi, ich bin " + firstName + " " + familyName + "!"); // -- Hi, ich bin
+*/
+/*
+let test;
+console.log("Inhalt: " + test);
+console.log("Datentyp: " +typeof test); // Ausgabe Datentyp
+console.log("------------");
+
+test = "hi"; // "hi " string
+console.log("Inhalt: " + test);
+console.log("Datentyp: " +typeof test); // Ausgabe Datentyp
+console.log("------------");
+
+test = 2; // number
+console.log("Inhalt: " + test);
+console.log("Datentyp: " +typeof test); // Ausgabe Datentyp
+console.log("------------");
+
+test = true; // number
+console.log("Inhalt: " + test);
+console.log("Datentyp: " +typeof test); // Ausgabe Datentyp
+console.log("------------");
+*/
+
+// variable vs. konstante
+
+// variable
+let test; // Variable
+test = "hi"; // wertweisung
+test = "Hello"; // Übeschreiben 
+console.log("Inhalt: " + test); // Ausgabe
+
+// Konstante
+const test2 = "Hi!?";
+test2 = "Hello";
+console.log("Inhalt: " + test2);
+
+
+
+
+
+
+
+
