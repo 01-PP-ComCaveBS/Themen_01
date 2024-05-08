86b1eec469888ce12fad2cf419279419d35a5296
Mahmoud3mood <168723599+Mahmoud3mood@users.noreply.github.com>
2024-05-08 08:34:47 +0000

    Deklaration

---
+++ src/02_variablen.js
@@ -0,0 +1,18 @@
+
+/** *
+ * 
+ * block-kommentar
+ * 
+ * /
+ * 
+
+*/
+// console.log("HI")
+// console.log(HI)
+
+let firstName; // Deklaration ( Definition)
+console.log(firstName); // Ausgabe
+firstName = "Max"; // wertzuweisung - assignment
+console.log(firstName); // Ausgabe
+let familyName = "Mütze";
+console.log("Hi, ich bin " + firstName + " " + familyName + "!"); // -- Hi, ich bin
