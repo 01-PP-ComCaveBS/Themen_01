d5ada42414327451795aa2aadc4659b2a9cf8eb1
Mahmoud3mood <168723599+Mahmoud3mood@users.noreply.github.com>
2024-05-13 10:19:13 +0000

    // 03a. Vorbereitung -Trennen der Funktionalitäten

--- src/08_funktionen_I.js
+++ src/08_funktionen_I.js
@@ -37,12 +37,43 @@ function outputNames()
 /***** Funktionen 02b *****/
 // 2b. Parametrisierung + Datenübergabe von AUSSEN
 
-outputNames2("Esther"); // Argument --> Daten für AUSSEN
+/*outputNames2("Esther"); // Argument --> Daten für AUSSEN
 outputNames2("Jan");
+outputNames2();
+*/
 
-
-function outputNames2(firstName) 
+function outputNames2(firstName) // Parameter
 {
         console.log("Hallo, " + firstName + "!");  
 }
 
+/***** Funktionen 02c *****/
+// 2c. Mehrere Parameter / Argumente
+//outputNames3("Maxine","Mützerich");
+
+//const prompt = require('prompt-sync')({sigint: true});
+//outputNames3(prompt("Vorname? : "),prompt("Nachname? : "),prompt("wie alt bist du? : "))   // Piping
+function outputNames3(firstName, familyName) // Parameter
+{
+    console.log("Hallo, " + firstName + " " + familyName + "!");
+
+}
+
+/***** Funktionen 03a *****/
+// 03a. Vorbereitung -Trennen der Funktionalitäten
+// Postulat: one function = one job (uncle Bob)
+// SRP single responsibility principle
+
+outputNamesSRP("Maxine","Mützerich");
+
+function outputNamesSRP(firstName, familyName) { // Parameter 
+
+    // 1. Funktionalität: string composing
+const GAP = " ";
+let outpuStr = "Hallo, " + firstName + GAP + familyName + "!"
+
+
+    // 2. Funktionalität: string output
+    console.log(outpuStr);
+
+}
