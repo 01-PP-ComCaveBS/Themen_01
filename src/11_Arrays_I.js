1abb72667e1089cb38830420b48cb63eb73246bb
Mahmoud3mood <168723599+Mahmoud3mood@users.noreply.github.com>
2024-05-15 09:59:42 +0000

    Transponierung

--- src/11_Arrays_I.js
+++ src/11_Arrays_I.js
@@ -5,15 +5,52 @@
  *   THEORIE
  * 
  */
-let arr;
+//let arr;
 //arr = new Array(); // konstruktor
 //arr = [];
-arr = [2,22,45,6,21];  //Array mit Elemnten
+//arr = [2,22,45,6,21];  //Array mit Elemnten
 
-output(arr);
-output(arr.length); // Anzahl die Elemente
-output(arr[0]); //index 0(erste Pos ist 0)
-output(arr[arr.length-1]); // letze POs.// allgemein .. n-1
+//output(arr);
+//output(arr.length); // Anzahl die Elemente
+//output(arr[0]); //index 0(erste Pos ist 0)
+//output(arr[arr.length-1]); // letze POs.// allgemein .. n-1
+
+
+
+/* 02a. Theorie: Schleifen (for-schleife) */
+/* For -Schleifen als allg. Wiederholungs-Struktur */
+
+
+// Inkrement (untere Grenze -->obere Genze)
+//for (let i = 0; i < 5; i++) {   // i = schleifenindex  , ++ Inkrement
+    
+ //  output("index: " + i);
+//}
+
+// dekrement (untere Grenze -->obere Genze)
+//for (let i = 10; i > 0; i--) {   // i = schleifenindex  , ++ Inkrement
+    
+   // output("index: " + i);
+//}
+
+// Inkrement (var. Schrittweite)
+//for (let i = 0; i <= 100; i+= 5) {   // i = schleifenindex  
+    
+ //   output("index: " + i);
+ // }
+
+
+ /* 02b. For-Schleife für Array-Index (Iteration)*/
+
+ //let arr = ["Ich","bin","die","coole","Maxine","Mützerich"];
+ //for (let i = 0; i < arr.length; i++) {   // i = schleifenindex 
+    
+   //  output("intern: " + i);
+  //   output("extern: " + arr[i]); // Mapping
+ // }
+
+
+
 
 /**
  *   PRAXIS
@@ -40,6 +77,72 @@ function getSentence(word1,word2,word3,word4,word5,word6) {
 }
 
 
+/*** 01a. Funktionalität mit Array 1 */
+// Kritik | Überlegung
+/*
+function getSentenceArr(arr) {
+
+
+}
+
+output(getSentence2(["Ich","bin","die","coole","Maxine","Mützerich"]));
+function getSentence2(arr) {
+    const GAP = " ";
+    const PUNCT = ".";
+    for (let i = 0; i < arr.length; i++) {   // i = schleifenindex 
+    
+          output(arr[i]);
+         
+      }
+     
+}
+
+}
+
+*/
+/*
+1. Einer Variablen kann Ihr eigener Wert zugewiesen werden.
+Solange die Variable existiert, bleibt dieser erhalten.
+hier: Aufsummierung
+*/
+/* Zu "Fuß" - DRY !!! */
+ //let a = 0; // Anfangswert
+ //output("inhalt von a: " + a);
+ // a = a + 1; // a = 0 + 1
+ //output("inhalt von a: " + a);
+ //a = a + 1; // a = 1 + 1
+ //output("inhalt von a: " + a);
+ //a = a + 1; // a = 2 + 1
+ //output("inhalt von a: " + a);
+
+ /* Besser: mit FOR-Schleife */
+ 
+ //let a = 0; // Anfangswert
+//for (let i = 0; i < 5; i++) {
+  //  a = a + 1;
+  //  output("inhalt von a: " + a);
+//}
+
+
+
+/*
+1a. Einer Variablen kann Ihr eigener Wert zugewiesen werden
+Solange die Variable existiert, bleibt dieser erhalten
+hier: Verkettung eines Strings // Transponierung
+*/
+
+let a = ""; // Anfangswert
+for (let i = 0; i < 5; i++) {
+    a = a + "test";
+    output("inhalt von a: " + a);
+}
+
+
+
+
+
+
+
 
 
 // Modul: Ausgabe in Konsole : Test
@@ -47,5 +150,7 @@ function getSentence(word1,word2,word3,word4,word5,word6) {
 function output(outputStr) {
     console.log(outputStr);
 }
+
+
 
 
